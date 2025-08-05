881f460fbdf230713512065a3d310dd8ffc92ec2
arielashari <arielashari00@gmail.com>
2025-08-05 19:02:23 +0800

    socket

--- app/socket.ts
+++ app/socket.ts
@@ -3,4 +3,6 @@
 import { io } from "socket.io-client";
 import {config} from "@/config/app";
 
-export const socket = io(config.baseUrl);
+export const socket = io("wss://c4cc0bfe7a09.ngrok-free.app", {
+    transports: ["websocket"],
+});
